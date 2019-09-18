b481b26a010c6f0e9e3562b3e1494d64b4edb787
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2019-09-18 14:41:05 -0400

    working on burner process only started the .C

---
+++ burners/GammaClusBurner.C
@@ -0,0 +1,82 @@
+#include "GammaClusBurner.h"
+#include <calobase/RawCluster.h>
+#include <phool/getClass.h>
+#include <TMath.h>
+#include <iostream>
+using namespace std;
+GammaClusBurner::GammaClusBurner(const std::string &name, unsigned runnumber=0,bool isHI=false) : SubsysReco("GammaClusBurner"),
+	_kRunNumber(runnumber), _kISHI(isHI)
+{
+	_foutname = name;
+}
+
+GammaClusBurner::~GammaClusBurner(){
+	if (_f) delete _f;
+}
+
+int GammaClusBurner::InitRun(PHCompositeNode *topNode)
+{
+
+	_f = new TFile( _foutname.c_str(), "RECREATE");
+	_ttree = new TTree("subtractedTree","super stylish sapling");
+  _ttree->Branch("sub_clus_n",&_b_clustersub_n);
+  _ttree->Branch("tphoton_e",&_b_truthphoton_E,"tphoton_e[sub_clus_n]/F");
+  _ttree->Branch("sub_clus_e",&_b_clustersub_E,"sub_clus_e[sub_clus_n]/F");
+  _ttree->Branch("sub_clus_ecore",&_b_clustersub_ecore,"sub_clus_ecore[sub_clus_n]/F");
+  _ttree->Branch("sub_clus_eta",&_b_clustersub_eta,"sub_clus_eta[sub_clus_n]/F");
+  _ttree->Branch("sub_clus_phi",&_b_clustersub_phi,"sub_clus_phi[sub_clus_n]/F");
+
+	return 0;
+}
+
+bool GammaClusBurner::doNodePointers(PHCompositeNode* topNode){
+	bool goodPointers=true;
+  //use subtracted clusters for HI events
+	if(_kISHI)_subClusterContainer = findNode::getClass<RawClusterContainer>(topNode,"CLUSTER_CEMC_SUB");
+  else _subClusterContainer = findNode::getClass<RawClusterContainer>(topNode,"CLUSTER_CEMC");
+	_truthinfo = findNode::getClass<PHG4TruthInfoContainer>(topNode,"G4TruthInfo");
+	if(!_subClusterContainer){
+		cerr<<Name()<<": critical error-bad nodes \n";
+		if(!_subClusterContainer){
+			cerr<<"\t RawClusterContainer is bad";
+		}
+		cerr<<endl;
+		goodPointers=false;
+	}
+	return goodPointers;
+}
+
+int GammaClusBurner::process_event(PHCompositeNode *topNode)
+{
+	doNodePointers(topNode);
+	PHG4TruthInfoContainer::Range range = _truthinfo->GetParticleRange(); //look at all truth particles
+	for ( PHG4TruthInfoContainer::ConstIterator iter = range.first; iter != range.second; ++iter ) {
+		PHG4Particle* g4particle = iter->second;
+    //maybe I am double counting due to kinematic updates
+    if(g4particle->get_pid()==22){
+      TLorentzVector gamma_tlv;
+      gamma_tlv.SetPxPyPzE(g4particle->get_px(),g4particle->get_py(),g4particle->get_pz(),g4particle->get_e());
+      if(gamma_tlv.Pt()<_kMINCLUSTERENERGY||TMath::Abs(gamma_tlv.Eta)>_kMAXETA) continue;
+      getCluster(&gamma_tlv);
+    }
+  return 0;
+}
+
+RawCluster* GammaClusBurner::getCluster(TLorentzVector* tlv){
+	RawClusterContainer::ConstRange begin_end = _subClusterContainer->getClusters(); 
+	RawClusterContainer::ConstIterator rtiter;
+	for (rtiter = begin_end.first; rtiter != begin_end.second; ++rtiter) 
+	{ 
+		RawCluster *cluster = rtiter->second; 
+    if(DeltaR(tlv,cluster)<_kCLUSTERDR) return cluster;
+  }
+}
+
+int GammaClusBurner::End(PHCompositeNode *topNode)
+{
+	cout<<"closing"<<endl;
+  _ttree->Write();
+	_f->Write();
+	_f->Close();
+	return 0;
+}
---
+++ burners/GammaClusBurner.h
@@ -0,0 +1,79 @@
+/**
+ * @GammaClusBurner.h
+ * @author Francesco Vassalli <Francesco.Vassalli@colorado.edu>
+ * @version 1.0
+ *
+ * @section make photon cluster E spec
+ */
+#ifndef GAMMACLUSBURNER_H__
+#define GAMMACLUSBURNER_H__
+
+#include <fun4all/SubsysReco.h>
+#include <calobase/RawClusterContainer.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <string>
+
+class PHCompositeNode;
+
+class GammaClusBurner: public SubsysReco
+{
+
+  public:
+
+    GammaClusBurner(const std::string &name,unsigned run,bool isHI);
+    ~GammaClusBurner();
+    int InitRun(PHCompositeNode*);
+
+    int process_event(PHCompositeNode*);
+    int End(PHCompositeNode*);
+
+
+  private:
+    inline double DeltaPhi (double phi1, double phi2, const bool sign=0) {
+      phi1 = (phi1);
+      phi2 = (phi2);
+      double dphi = abs(phi1 - phi2);
+      while (dphi > TMath::Pi()) dphi = abs (dphi - 2*TMath::Pi());
+
+      if (sign &&  (phi2 + dphi) == phi1)
+        dphi *= -1;
+
+      return dphi;
+    }
+
+    inline double DeltaR (const double eta1, const double eta2, const double phi1, const double phi2 ) {
+      const double deta = eta1 - eta2;
+      const double dphi = DeltaPhi (phi1, phi2, false);
+      return sqrt( pow( deta, 2 ) + pow( dphi, 2 ) );
+    }
+    inline double DeltaR (TLorentzVector *tlv, RawCluster* cluster){
+      float cluseta=-1 * log( tan( TMath::ATan2( cluster->get_r(), cluster->get_z()  ) / 2.0 ) );
+      return DeltaR(tlv->Eta(),cluseta,tlv->Phi(),cluster->get_phi());
+    }
+
+    bool doNodePointers(PHCompositeNode* topNode);
+
+    const unsigned int _kRunNumber;
+    const bool _kISHI;
+    static const unsigned int _kMINCLUSTERENERGY=2;//in GeV
+    static const unsigned int _kMAXCLUSTERS=400;
+    TFile *_f=NULL; ///< output file
+    TTree *_ttree=NULL; ///<signal data for making track pair cuts
+    RawClusterContainer* _subClusterContainer=NULL;
+    std::string _foutname;
+
+    float _b_truthPhoton_E   [ _kMAXCLUSTERS ];
+    float _b_clustersub_E    [ _kMAXCLUSTERS ];
+    float _b_clustersub_ecore[ _kMAXCLUSTERS ] ;
+    float _b_clustersub_eta  [ _kMAXCLUSTERS ];
+    float _b_clustersub_phi  [ _kMAXCLUSTERS ];
+    unsigned _b_clustersub_n;                            
+};
+
+
+
+#endif // __GAMMACLUSBURNER_H__
+
+
+

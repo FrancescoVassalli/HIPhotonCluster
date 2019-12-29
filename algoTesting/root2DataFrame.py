b26fa1c3034f4878273cf4f5a78c5b773634c73e
FrancescoVassalli <FrancescoVassalli@colorado.edu>
2019-12-29 16:06:51 -0700

    does df

--- algoTesting/root2DataFrame.py
+++ algoTesting/root2DataFrame.py
@@ -1,67 +1,68 @@
 
 class PhotonCluster:
 	def __init__(self):
-		total_energy =0
-		core_energy=0
+		self.total_energy =0
+		self.core_energy=0
 		for x in range(0,49):
-		 	towers.append(0)
+		 	self.towers.append(0)
 	def __init__(self,treeEvent,i):
-		total_energy = treeEvent.sub_clus_e[i]
-		core_energy = treeEvent.sub_clus_ecore[i]
-		towers=[]
+		self.total_energy = treeEvent.sub_clus_e[i]
+		self.core_energy = treeEvent.sub_clus_ecore[i]
+		self.towers=[]
 		#print(total_energy)
-		towers.append(treeEvent.tower0[i])
-		towers.append(treeEvent.tower1[i])
-		towers.append(treeEvent.tower2[i])
-		towers.append(treeEvent.tower3[i])
-		towers.append(treeEvent.tower4[i])
-		towers.append(treeEvent.tower5[i])
-		towers.append(treeEvent.tower6[i])
-		towers.append(treeEvent.tower7[i])
-		towers.append(treeEvent.tower8[i])
-		towers.append(treeEvent.tower9[i])
-		towers.append(treeEvent.tower10[i])
-		towers.append(treeEvent.tower11[i])
-		towers.append(treeEvent.tower12[i])
-		towers.append(treeEvent.tower13[i])
-		towers.append(treeEvent.tower14[i])
-		towers.append(treeEvent.tower15[i])
-		towers.append(treeEvent.tower16[i])
-		towers.append(treeEvent.tower17[i])
-		towers.append(treeEvent.tower18[i])
-		towers.append(treeEvent.tower19[i])
-		towers.append(treeEvent.tower20[i])
-		towers.append(treeEvent.tower21[i])
-		towers.append(treeEvent.tower22[i])
-		towers.append(treeEvent.tower23[i])
-		towers.append(treeEvent.tower24[i])
-		towers.append(treeEvent.tower25[i])
-		towers.append(treeEvent.tower26[i])
-		towers.append(treeEvent.tower27[i])
-		towers.append(treeEvent.tower28[i])
-		towers.append(treeEvent.tower29[i])
-		towers.append(treeEvent.tower30[i])
-		towers.append(treeEvent.tower31[i])
-		towers.append(treeEvent.tower32[i])
-		towers.append(treeEvent.tower33[i])
-		towers.append(treeEvent.tower34[i])
-		towers.append(treeEvent.tower35[i])
-		towers.append(treeEvent.tower36[i])
-		towers.append(treeEvent.tower37[i])
-		towers.append(treeEvent.tower38[i])
-		towers.append(treeEvent.tower39[i])
-		towers.append(treeEvent.tower40[i])
-		towers.append(treeEvent.tower41[i])
-		towers.append(treeEvent.tower42[i])
-		towers.append(treeEvent.tower43[i])
-		towers.append(treeEvent.tower44[i])
-		towers.append(treeEvent.tower45[i])
-		towers.append(treeEvent.tower46[i])
-		towers.append(treeEvent.tower47[i])
-		towers.append(treeEvent.tower48[i])
-	def getRow(self):
-		r = [self.total_energy,self.core_energy]
-		r.extend(self.towers)
+		self.towers.append(treeEvent.tower0[i])
+		self.towers.append(treeEvent.tower1[i])
+		self.towers.append(treeEvent.tower2[i])
+		self.towers.append(treeEvent.tower3[i])
+		self.towers.append(treeEvent.tower4[i])
+		self.towers.append(treeEvent.tower5[i])
+		self.towers.append(treeEvent.tower6[i])
+		self.towers.append(treeEvent.tower7[i])
+		self.towers.append(treeEvent.tower8[i])
+		self.towers.append(treeEvent.tower9[i])
+		self.towers.append(treeEvent.tower10[i])
+		self.towers.append(treeEvent.tower11[i])
+		self.towers.append(treeEvent.tower12[i])
+		self.towers.append(treeEvent.tower13[i])
+		self.towers.append(treeEvent.tower14[i])
+		self.towers.append(treeEvent.tower15[i])
+		self.towers.append(treeEvent.tower16[i])
+		self.towers.append(treeEvent.tower17[i])
+		self.towers.append(treeEvent.tower18[i])
+		self.towers.append(treeEvent.tower19[i])
+		self.towers.append(treeEvent.tower20[i])
+		self.towers.append(treeEvent.tower21[i])
+		self.towers.append(treeEvent.tower22[i])
+		self.towers.append(treeEvent.tower23[i])
+		self.towers.append(treeEvent.tower24[i])
+		self.towers.append(treeEvent.tower25[i])
+		self.towers.append(treeEvent.tower26[i])
+		self.towers.append(treeEvent.tower27[i])
+		self.towers.append(treeEvent.tower28[i])
+		self.towers.append(treeEvent.tower29[i])
+		self.towers.append(treeEvent.tower30[i])
+		self.towers.append(treeEvent.tower31[i])
+		self.towers.append(treeEvent.tower32[i])
+		self.towers.append(treeEvent.tower33[i])
+		self.towers.append(treeEvent.tower34[i])
+		self.towers.append(treeEvent.tower35[i])
+		self.towers.append(treeEvent.tower36[i])
+		self.towers.append(treeEvent.tower37[i])
+		self.towers.append(treeEvent.tower38[i])
+		self.towers.append(treeEvent.tower39[i])
+		self.towers.append(treeEvent.tower40[i])
+		self.towers.append(treeEvent.tower41[i])
+		self.towers.append(treeEvent.tower42[i])
+		self.towers.append(treeEvent.tower43[i])
+		self.towers.append(treeEvent.tower44[i])
+		self.towers.append(treeEvent.tower45[i])
+		self.towers.append(treeEvent.tower46[i])
+		self.towers.append(treeEvent.tower47[i])
+		self.towers.append(treeEvent.tower48[i])
+	def getDict(self):
+		r = {"total_energy": self.total_energy, 'core_energy':self.core_energy}
+		for i in range(0,len(self.towers)):
+			r["tower"+str(i)] = self.towers[i]
 		return r
 
 def makePhotonClusters(chain):
@@ -96,6 +97,13 @@ def makeDataFrame(l_cluster):
 	for cluster in l_cluster:
 		df.append(pd.DataFrame(columns=names,data=cluster.getRow()),ignore_index=True)
 	return df
+
+def makeDataFrame(l_cluster):
+	rows = []
+	for cluster in l_cluster:
+		rows.append(cluster.getDict())
+	return pd.DataFrame(rows)
+	
 
 
 

e6206ccddc8ca59b397ee139aad0e64a32e9e115
FrancescoVassalli <francescovassalli@engr2-2-70-99-edu.int.colorado.edu>
2020-02-19 11:21:42 -0700

    syntax

--- analysis/PromptRatio.C
+++ analysis/PromptRatio.C
@@ -1,9 +1,12 @@
 int PromptRatio(){
-  TFile* data = new TFile("pythadata.root","READ");
-  TFile* output = new TFile("promptana.root","WRITE");
-  TTree* tree = data->Get("tree");
+  TFile* output = new TFile("promptana.root","CREATE");
+  TChain *tree = new TChain("tree");
+  tree->Add("pythdata.root");
   TH1F *pHist = new TH1F("pHist","",2,-.5,1.5);
   tree->Draw("prompt >> pHist","pid==22");
+  pHist->Scale(1/pHist->Integral());
+  //pHist->Draw();
   pHist->Write();
-  output->Close();
+  //output->Close();
+  return 0;
 }

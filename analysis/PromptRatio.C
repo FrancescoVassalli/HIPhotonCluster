fdd1a803fad86cf626696c9dc859ad59442eedaa
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2020-02-22 17:40:27 -0500

    use Teff for pt binned prompt rate

--- analysis/PromptRatio.C
+++ analysis/PromptRatio.C
@@ -2,11 +2,13 @@ int PromptRatio(){
   TFile* output = new TFile("promptana.root","CREATE");
   TChain *tree = new TChain("tree");
   tree->Add("pythdata.root");
-  TH1F *pHist = new TH1F("pHist","",2,-.5,1.5);
-  tree->Draw("prompt >> pHist","pid==22");
-  pHist->Scale(1/pHist->Integral());
-  //pHist->Draw();
-  pHist->Write();
+  TH1F *all = new TH1F("all","",8,5,20);
+  tree->Draw("energy >> all","pid==22");
+  TH1F *prompt = new TH1F("prompt","",8,5,20);
+  tree->Draw("energy >> prompt","prompt==1");
+  TEfficiency* promptRate = new TEfficiency(prompt,all);
+  promptRate->Draw();
+  promptRate->Write();
   //output->Close();
   return 0;
 }

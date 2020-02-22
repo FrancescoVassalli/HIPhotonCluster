537380a73beeee6bd59108e51671dd88cb3cd019
FrancescoVassalli <francescovassalli@engr2-2-70-99-edu.int.colorado.edu>
2020-02-22 16:14:37 -0700

    rebin

--- analysis/PromptRatio.C
+++ analysis/PromptRatio.C
@@ -2,11 +2,11 @@ int PromptRatio(){
   TFile* output = new TFile("promptana.root","CREATE");
   TChain *tree = new TChain("tree");
   tree->Add("pythdata.root");
-  TH1F *all = new TH1F("all","",8,5,20);
+  TH1F *all = new TH1F("all","",5,5,14);
   tree->Draw("energy >> all","pid==22");
-  TH1F *prompt = new TH1F("prompt","",8,5,20);
+  TH1F *prompt = new TH1F("prompt","",5,5,14);
   tree->Draw("energy >> prompt","prompt==1");
-  TEfficiency* promptRate = new TEfficiency(prompt,all);
+  TEfficiency* promptRate = new TEfficiency(*prompt,*all);
   promptRate->Draw();
   promptRate->Write();
   //output->Close();

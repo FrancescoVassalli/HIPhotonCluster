da2fe83f35b615d55233ec0f328a6b96ee57f8b7
FrancescoVassalli <francescovassalli@engr2-2-70-99-edu.int.colorado.edu>
2020-02-21 17:06:49 -0700

    new tree structure

--- analysis/ClusterPlotter.C
+++ analysis/ClusterPlotter.C
@@ -13,24 +13,16 @@ pair<int,int> getTowerEnergy(unsigned mapPosition){
 void makeMaps(TChain *tree,string ext="",unsigned nclusters=0){
   const unsigned kNTOWERS = 49;
 	int clusn;
-  float gammaE[600];
   float clusE[600];
-  float matchDR[600];
-  float matchDPhi[600];
-  float matchDEta[600];
   float prob[600];
   float towers[kNTOWERS][600];
 
-  tree->SetBranchAddress("tphoton_e",gammaE);
-  tree->SetBranchAddress("sub_clus_n",&clusn);
-  tree->SetBranchAddress("sub_clus_e",clusE);
-  tree->SetBranchAddress("matchDR",matchDR);
-  tree->SetBranchAddress("matchPhi",matchDPhi);
-  tree->SetBranchAddress("matchEta",matchDEta);
-  tree->SetBranchAddress("sub_clus_prob",prob);
+  tree->SetBranchAddress("clus_n",&clusn);
+  tree->SetBranchAddress("clus_e",clusE);
+  tree->SetBranchAddress("clus_prob",prob);
 
   string bTitle="tower";
-  string bStruct = "[sub_clus_n]/F";
+  string bStruct = "[clus_n]/F";
   for (unsigned i = 0; i < kNTOWERS; ++i)
   {
     string str = bTitle + std::to_string(i);
@@ -50,7 +42,7 @@ void makeMaps(TChain *tree,string ext="",unsigned nclusters=0){
     }
     string name = strcluster + std::to_string(nclusters);
     std::pair<int,int> loc;
-    TH2F *map= new TH2F(name.c_str(),std::to_string(gammaE[iarray]).c_str(),7,-.5,6.5,7,-.5,6.5);
+    TH2F *map= new TH2F(name.c_str(),,7,-.5,6.5,7,-.5,6.5);
     for (unsigned i = 0; i < kNTOWERS; ++i)
     {
       loc = getTowerEnergy(i);
@@ -64,16 +56,16 @@ void makeMaps(TChain *tree,string ext="",unsigned nclusters=0){
     }
     nclusters--;
   }
-  TH2F *test = new TH2F("test","",7,-.5,6.5,7,-.5,6.5);
+  /*TH2F *test = new TH2F("test","",7,-.5,6.5,7,-.5,6.5);
   test->SetBinContent(4,4,1);
-  test->Write();
+  test->Write();*/
 }
 
 
 
 void ClusterPlotter(){
 	string inName="/sphenix/user/vassalli/idTest/HIsample/hipytha_ANA0.root";
-	TFile *f_data = new TFile("towerData.root","UPDATE");
+	TFile *f_data = new TFile("towerData.root","CREATE");
 	TChain *tree = new TChain("subtractedTree");
 	tree->Add(inName.c_str());
 	makeMaps(tree,"",5);
